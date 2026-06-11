0e0fa842e77822b5fe21184295b855d117176646
Nathan Robinson <nathan.robinson007@gmail.com>
2026-06-11 17:31:07 +0000

    File changes

--- src/components/goals/TopNav.jsx
+++ src/components/goals/TopNav.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const NAV_ITEMS = [
-  { label: 'Household', path: '/' },
+  { label: 'Household', path: '/household' },
   { label: 'Goals',     path: '/goals' },
   { label: 'Scenarios', path: '/scenarios' },
   { label: 'Sequencing',path: '/sequencing' },
